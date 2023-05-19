501f79ba1f60b6fbb8cb97f9d7ca73395f575d16
VVarren <warrenwu8888@gmail.com>
2023-05-18 22:31:45 -0400

    innit branch

--- website/my-app/src/App.js
+++ website/my-app/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>START PROJECT</h1>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<img
+					id="logo"
+					src="https://i0.wp.com/news.xbox.com/en-us/wp-content/uploads/sites/2/2021/12/Hero.jpg?resize=1200%2C675&ssl=1>"
+				/>
+				<span id="left-text">Some text</span>
+				<span id="right-text">Some other text</span>
+			</header>
+
+			<div id="middle-row">
+				<div class="middle-box"></div>
+				<div class="middle-box"></div>
+			</div>
+
+			<div id="bottom-row">
+				<div class="graph"></div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
